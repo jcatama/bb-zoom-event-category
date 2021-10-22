cbf26c842d8b9d135b422fe05f8db90cb492565f
Albert Catama <jcatama@katanatechworks.com>
2021-10-22 13:10:50 +0800

    Styling fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     $('.zoom-group-calendar').on('click', function(e) {
 
         let group_calendar_slug = ( $( this )[0].classList[2]).trim();
-        window.location = bbzec.home_url + '/events/category/' + group_calendar_slug;
+        window.open( bbzec.home_url + '/events/category/' + group_calendar_slug, '_blank');
 
     });
 
